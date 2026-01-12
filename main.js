891f693173fae78300ea7bcae426e1ee2be70406
DV155 <dan0024175@gmail.com>
2026-01-13 00:01:33 +0100

    Expanded logic for the field

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 yourMove = false;
+
+
+function chooseX() {
+    let choiceVar = "cross";
+}
+
+function chooseO() {
+    let choiceVar = "circle";
+}
+
+if (yourMove = true) && (choiceVar = "cross") {
+    document.getElementById("1b").addEventListener("click", addCross);
+    document.getElementById("2b").addEventListener("click", addCross);
+    document.getElementById("3b").addEventListener("click", addCross);
+    document.getElementById("4b").addEventListener("click", addCross);
+    document.getElementById("5b").addEventListener("click", addCross);
+    document.getElementById("6b").addEventListener("click", addCross);
+    document.getElementById("1b").addEventListener("click", addCross);
+    document.getElementById("8b").addEventListener("click", addCross);
+    document.getElementById("9b").addEventListener("click", addCross);
+};
+
+if (yourMove = true) && (choiceVar = "circle") {
+    document.getElementById("1b").addEventListener("click", addCircle);
+    document.getElementById("2b").addEventListener("click", addCircle);
+    document.getElementById("3b").addEventListener("click", addCircle);
+    document.getElementById("4b").addEventListener("click", addCircle);
+    document.getElementById("5b").addEventListener("click", addCircle);
+    document.getElementById("6b").addEventListener("click", addCircle);
+    document.getElementById("1b").addEventListener("click", addCircle);
+    document.getElementById("8b").addEventListener("click", addCircle);
+    document.getElementById("9b").addEventListener("click", addCircle);
+};
